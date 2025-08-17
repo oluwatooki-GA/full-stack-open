fc38518468c77b8bd1a9227f427b823997b86fce
oluwatooki-GA <oluwatooki.g.adegbite@gmail.com>
2025-08-17 19:16:29 +0100

    commit 4.1 - 4.2

---
+++ part4/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const blogRouter = require('./controllers/blog')
+
+const app = express()
+app.use(express.json())
+// app.use(cors())
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose
+    .connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connection to MongoDB:', error.message)
+    })
+
+
+const PORT = config.PORT
+
+app.use('/api/blog', blogRouter)
+
+module.exports = app
