5f739b070a90d4f118f39df3ad421748596314e1
oluwatooki-GA <oluwatooki.g.adegbite@gmail.com>
2025-08-19 23:37:32 +0100

    commit 4.20*: Blog List Expansion, step 8

--- part4/controllers/blog.js
+++ part4/controllers/blog.js
@@ -3,13 +3,7 @@ const Blog = require('../models/blog')
 const User = require("../models/user");
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.startsWith('Bearer ')) {
-        return authorization.replace('Bearer ', '')
-    }
-    return null
-}
+
 
 blogRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user',{ username:1,name:1 })
@@ -23,7 +17,8 @@ blogRouter.post('/', async (request, response) => {
         return response.status(400).end()
     }
 
-    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
     if (!decodedToken.id) {
         return response.status(401).json({ error: 'token invalid' })
     }
--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -1,15 +1,23 @@
 const logger = require('./logger')
 
 const requestLogger = (request, response, next) => {
-  logger.info('Method:', request.method)
-  logger.info('Path:  ', request.path)
-  logger.info('Body:  ', request.body)
-  logger.info('---')
-  next()
+    logger.info('Method:', request.method)
+    logger.info('Path:  ', request.path)
+    logger.info('Body:  ', request.body)
+    logger.info('---')
+    next()
+}
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        request.token =  authorization.replace('Bearer ', '')
+    }
+    next()
 }
 
+
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+    response.status(404).send({ error: 'unknown endpoint' })
 }
 
 const errorHandler = (error, request, response, next) => {
@@ -28,14 +36,12 @@ const errorHandler = (error, request, response, next) => {
             error: 'token expired'
         })
     }
-
-
-
     next(error)
 }
 
 module.exports = {
-  requestLogger,
-  unknownEndpoint,
-  errorHandler,
+    requestLogger,
+    unknownEndpoint,
+    errorHandler,
+    tokenExtractor
 }
