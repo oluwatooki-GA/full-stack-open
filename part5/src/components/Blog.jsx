e9e330e073c1ecdff7a81066cd3c8e5960ddd9f0
oluwatooki-GA <oluwatooki.g.adegbite@gmail.com>
2025-08-20 18:12:25 +0100

    add 5.7 Blog List Frontend, step 7

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,25 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import {useState} from "react";
+import blogService from "../services/blogs.js"
+const Blog = ({blog}) => {
+    const [visible, setVisible] = useState(false)
+    const [likes, setLikes] = useState(blog.likes)
+    const updateLikes = async () => {
+        await blogService.updateLikes(blog.id, likes + 1)
+        setLikes(likes + 1)
+    }
+    return (
+        <div style={{ border:'2px solid black', padding:5,marginBottom:15 }}>
+            {blog.title} {blog.author} <button onClick={() => setVisible(!visible)}>{ visible ? 'hide':'view' }</button>
+            {visible && (
+                <div>
+                    <p>url: {blog.url}</p>
+                    <p>likes: {likes} <button onClick={updateLikes}>like</button></p>
+                    <p>username: {blog.user.username}</p>
+                </div>
+            )}
 
-export default Blog
+        </div>
+    );
+};
+
+export default Blog;
