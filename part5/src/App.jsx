fb1e3d40ed2cca978418fdfccb478f57c72b5ac1
oluwatooki-GA <oluwatooki.g.adegbite@gmail.com>
2025-08-20 14:49:55 +0100

    add 5.1: Blog List Frontend, step 1

---
+++ part5/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import LoginForm from "./components/LoginForm.jsx";
+
+const App = () => {
+    const [blogs, setBlogs] = useState([])
+
+    const [errorMessage, setErrorMessage] = useState(null)
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        blogService.getAll().then(blogs =>
+            setBlogs(blogs)
+        )
+    }, [])
+
+    useEffect(() => {
+        const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+        if (loggedUserJSON) {
+            const user = JSON.parse(loggedUserJSON)
+            setUser(user)
+            // noteService.setToken(user.token)
+        }
+    }, [])
+
+    console.log(user)
+    return (
+        <div>
+            <h2>blogs</h2>
+            <p>{errorMessage}</p>
+
+            { user ? <p>{user.username} is logged in</p> :  <LoginForm setUser={setUser} setErrorMessage={setErrorMessage} /> }
+            {blogs.map(blog =>
+                <Blog key={blog.id} blog={blog} />
+            )}
+        </div>
+    )
+}
+
+export default App
