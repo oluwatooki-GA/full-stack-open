350874541604b3a87b338e354869d252bd7159cf
oluwatooki-GA <oluwatooki.g.adegbite@gmail.com>
2025-08-21 13:20:24 +0100

    add 5.14: Blog List Tests, step 2

--- part5/src/components/Blog.test.jsx
+++ part5/src/components/Blog.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from "@testing-library/user-event";
 
 test('<Blog /> renders title and author', () => {
     const blog = {
@@ -25,4 +26,33 @@ test('<Blog /> renders title and author', () => {
 
     expect(screen.queryByText(/url:/)).toBeNull()
     expect(screen.queryByText(/likes:/)).toBeNull()
+})
+
+test('URL and likes are shown when the view button is clicked', async () => {
+    const blog = {
+        title: 'Testing React components',
+        author: 'Jane Doe',
+        url: 'http://example.com',
+        likes: 5,
+        user: {username: 'janedoe'}
+    }
+
+    render(
+        <Blog
+            blog={blog}
+            setErrorMessage={() => {
+            }}
+            username="janedoe"
+            setNotification={() => {
+            }}
+        />
+    )
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    // URL and likes should now be visible
+    expect(screen.getByText(/url: http:\/\/example.com/, {exact: false})).toBeInTheDocument()
+    expect(screen.getByText(/likes: 5/,{ exact: false })).toBeInTheDocument()
 })
